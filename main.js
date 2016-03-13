d31ffe3eb168d7eb543d878ac2990f0fd7b105b2
Yu Qi <yq85@cornell.edu>
2016-03-12 23:04:07 -0500

    modal window problem

--- main.js
+++ main.js
@@ -53,18 +53,6 @@
         });
 
 
-            $(".img-responsive").click(function () {
-
-            // Set the effect type
-            var effect = 'slide';
-
-            // Set the options for the effect type chosen
-            var options = { direction: 'up' };
-
-            // Set the duration (default: 400 milliseconds)
-            var duration = 500;
-
-            $('#myDiv').toggle(effect, options, duration);
-            });
+            
 
 //    </script>
